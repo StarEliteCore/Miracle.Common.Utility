7a757649760c3bebdaf1ba7c492d498faac56f33
Joe_Du <dygood@outlook.com>
2019-01-03 14:10:00 +0800

    字符串加入全角半角,以及数字和其他正则的验证.

--- Miraclesoft.Common.Utility/Miraclesoft.Common.Utility.String/StringExtension.cs
+++ Miraclesoft.Common.Utility/Miraclesoft.Common.Utility.String/StringExtension.cs
@@ -46,17 +46,24 @@ namespace Miraclesoft.Common.Utility.String
         /// <returns>转换后的日期。</returns>
         public static DateTime ToDateTime(this string value, DateTime defaultValue)
         {
-            if (string.IsNullOrEmpty(value))
+            try
             {
-                return defaultValue;
+                if (string.IsNullOrEmpty(value))
+                {
+                    return defaultValue;
+                }
+                if (DateTime.TryParse(value, out defaultValue))
+                {
+                    return defaultValue;
+                }
+                else
+                {
+                    return defaultValue;
+                }
             }
-            if (DateTime.TryParse(value, out defaultValue))
+            catch (Exception e)
             {
-                return defaultValue;
-            }
-            else
-            {
-                return defaultValue;
+                throw e;
             }
         }
         #endregion
@@ -69,30 +76,45 @@ namespace Miraclesoft.Common.Utility.String
         /// <returns>转化后的字符串集合，如果传入数组为null则返回空集合。</returns>
         public static StringCollection ToStringCollection(this string[] value)
         {
-            StringCollection strColl = new StringCollection();
-            if (value != null)
+            try
             {
-                strColl.AddRange(value);
+                StringCollection strColl = new StringCollection();
+                if (value != null)
+                {
+                    strColl.AddRange(value);
+                }
+                return strColl;
             }
-            return strColl;
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
         #endregion
 
-        #region 以特定字符间隔的字符串转化为字符串集合。
+        #region 以特定字符间隔的字符串转化为字符串集合
+
         /// <summary>
-        /// 以特定字符间隔的字符串转化为字符串集合。
+        /// 以特定字符间隔的字符串转化为字符串集合
         /// </summary>
-        /// <param name="value">需要处理的字符串。</param>
-        /// <param name="separator">分隔此实例中子字符串的 Unicode 字符。</param>
-        /// <returns>转化后的字符串集合，如果传入数组为null则返回空集合。</returns>
+        /// <param name="value">需要处理的字符串</param>
+        /// <param name="separator">分隔此实例中子字符串的 Unicode 字符</param>
+        /// <returns>转化后的字符串集合，如果传入数组为null则返回空集合</returns>
         public static StringCollection ToStringCollection(this string value, char separator)
         {
-            StringCollection strColl = new StringCollection();
-            if (value == null)
+            try
             {
-                return strColl;
+                StringCollection strColl = new StringCollection();
+                if (value == null)
+                {
+                    return strColl;
+                }
+                return value.ToStringCollection(separator.ToString());
             }
-            return value.ToStringCollection(separator.ToString());
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
         #endregion
@@ -106,28 +128,35 @@ namespace Miraclesoft.Common.Utility.String
         /// <returns>转化后的字符串集合，如果传入数组为null则返回空集合。</returns>
         public static StringCollection ToStringCollection(this string value, string separator)
         {
-            StringCollection col = new StringCollection();
-            if (string.IsNullOrEmpty(separator) || string.IsNullOrEmpty(value) || string.IsNullOrEmpty(value.Trim()))
+            try
             {
+                StringCollection col = new StringCollection();
+                if (string.IsNullOrEmpty(separator) || string.IsNullOrEmpty(value) || string.IsNullOrEmpty(value.Trim()))
+                {
+                    return col;
+                }
+                int index = 0;
+                int pos = 0;
+                int len = separator.Length;
+                while (pos >= 0)
+                {
+                    pos = value.IndexOf(separator, index, StringComparison.CurrentCultureIgnoreCase);
+                    if (pos >= 0)
+                    {
+                        col.Add(value.Substring(index, pos - index));
+                    }
+                    else
+                    {
+                        col.Add(value.Substring(index));
+                    }
+                    index = pos + len;
+                }
                 return col;
             }
-            int index = 0;
-            int pos = 0;
-            int len = separator.Length;
-            while (pos >= 0)
+            catch (Exception e)
             {
-                pos = value.IndexOf(separator, index, StringComparison.CurrentCultureIgnoreCase);
-                if (pos >= 0)
-                {
-                    col.Add(value.Substring(index, pos - index));
-                }
-                else
-                {
-                    col.Add(value.Substring(index));
-                }
-                index = pos + len;
+                throw e;
             }
-            return col;
         }
         #endregion
 
@@ -137,14 +166,21 @@ namespace Miraclesoft.Common.Utility.String
         /// </summary>
         public static T JsonToObject<T>(this string value)
         {
-            var type = typeof(T);
-            var tReturn = type.Assembly.CreateInstance(type.FullName);
             try
             {
-                tReturn = JsonConvert.DeserializeObject<T>(value);
+                var type = typeof(T);
+                var tReturn = type.Assembly.CreateInstance(type.FullName);
+                try
+                {
+                    tReturn = JsonConvert.DeserializeObject<T>(value);
+                }
+                catch { }
+                return (T)tReturn;
             }
-            catch { }
-            return (T)tReturn;
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
         #endregion
 
@@ -156,17 +192,24 @@ namespace Miraclesoft.Common.Utility.String
         /// <returns></returns>
         public static string ToTitleUpperCase(this string value)
         {
-            Regex regex = new Regex(@"\w+");
-            return regex.Replace(value,
-                delegate (Match m)
-                {
-                    string str = m.ToString();
-                    if (char.IsLower(str[0]))
+            try
+            {
+                Regex regex = new Regex(@"\w+");
+                return regex.Replace(value,
+                    delegate (Match m)
                     {
-                        return char.ToUpper(str[0], System.Globalization.CultureInfo.CurrentCulture) + str.Substring(1, str.Length - 1);
-                    }
-                    return str;
-                });
+                        string str = m.ToString();
+                        if (char.IsLower(str[0]))
+                        {
+                            return char.ToUpper(str[0], System.Globalization.CultureInfo.CurrentCulture) + str.Substring(1, str.Length - 1);
+                        }
+                        return str;
+                    });
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
         /// <summary>
@@ -176,17 +219,24 @@ namespace Miraclesoft.Common.Utility.String
         /// <returns></returns>
         public static string ToTitleLowerCase(this string value)
         {
-            Regex regex = new Regex(@"\w+");
-            return regex.Replace(value,
-                delegate (Match m)
-                {
-                    string str = m.ToString();
-                    if (char.IsUpper(str[0]))
+            try
+            {
+                Regex regex = new Regex(@"\w+");
+                return regex.Replace(value,
+                    delegate (Match m)
                     {
-                        return char.ToLower(str[0], System.Globalization.CultureInfo.CurrentCulture) + str.Substring(1, str.Length - 1);
-                    }
-                    return str;
-                });
+                        string str = m.ToString();
+                        if (char.IsUpper(str[0]))
+                        {
+                            return char.ToLower(str[0], System.Globalization.CultureInfo.CurrentCulture) + str.Substring(1, str.Length - 1);
+                        }
+                        return str;
+                    });
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
         #endregion
 
@@ -198,14 +248,14 @@ namespace Miraclesoft.Common.Utility.String
         /// <returns>整数值，转换不成功返回0。</returns>
         public static int ToInt(this string value)
         {
-            int returnValue = 0;
             try
             {
-                returnValue = Convert.ToInt32(value);
+                return Convert.ToInt32(value);
             }
-            catch
-            { }
-            return returnValue;
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
         /// <summary>
@@ -231,11 +281,18 @@ namespace Miraclesoft.Common.Utility.String
         /// <returns>字节流。</returns>
         public static MemoryStream ToStream(this string value, Encoding encoding)
         {
-            MemoryStream mStream = new MemoryStream();
-            byte[] data = encoding.GetBytes(value);
-            mStream.Write(data, 0, data.Length);
-            mStream.Position = 0;
-            return mStream;
+            try
+            {
+                MemoryStream mStream = new MemoryStream();
+                byte[] data = encoding.GetBytes(value);
+                mStream.Write(data, 0, data.Length);
+                mStream.Position = 0;
+                return mStream;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
         /// <summary>
@@ -261,10 +318,17 @@ namespace Miraclesoft.Common.Utility.String
         /// <returns>字符串数组</returns>
         public static string[] Split(this string value, string separator)
         {
-            StringCollection collection = value.ToStringCollection(separator);
-            string[] vs = new string[collection.Count];
-            collection.CopyTo(vs, 0);
-            return vs;
+            try
+            {
+                StringCollection collection = value.ToStringCollection(separator);
+                string[] vs = new string[collection.Count];
+                collection.CopyTo(vs, 0);
+                return vs;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
 
         /// <summary>
@@ -280,7 +344,7 @@ namespace Miraclesoft.Common.Utility.String
             }
             catch (Exception ex)
             {
-                throw new Exception("输入的字符串无法转化为GUID对象", ex);
+                throw ex;
             }
         }
         #endregion
@@ -293,9 +357,16 @@ namespace Miraclesoft.Common.Utility.String
         /// <returns></returns>
         public static string StringToBase64(this string value)
         {
-            byte[] byteArray = Encoding.Default.GetBytes(value);
-            //先转成byte[];
-            return Convert.ToBase64String(byteArray);
+            try
+            {
+                byte[] byteArray = Encoding.Default.GetBytes(value);
+                //先转成byte[];
+                return Convert.ToBase64String(byteArray);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
         /// <summary>
         /// 将Base64字符转成String
@@ -304,8 +375,15 @@ namespace Miraclesoft.Common.Utility.String
         /// <returns></returns>
         public static string Base64ToString(this string value)
         {
-            byte[] byteArray = Convert.FromBase64String(value);
-            return Encoding.Default.GetString(byteArray);
+            try
+            {
+                byte[] byteArray = Convert.FromBase64String(value);
+                return Encoding.Default.GetString(byteArray);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
         #endregion
 
@@ -319,13 +397,121 @@ namespace Miraclesoft.Common.Utility.String
         /// <returns></returns>
         public static string Spacing(this string text, string spacingString, int spacingIndex)
         {
-            StringBuilder sb = new StringBuilder(text);
-            for (int i = spacingIndex; i <= sb.Length; i += spacingIndex + 1)
+            try
             {
-                if (i >= sb.Length) break;
-                sb.Insert(i, spacingString);
+                StringBuilder sb = new StringBuilder(text);
+                for (int i = spacingIndex; i <= sb.Length; i += spacingIndex + 1)
+                {
+                    if (i >= sb.Length) break;
+                    sb.Insert(i, spacingString);
+                }
+                return sb.ToString();
             }
-            return sb.ToString();
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        #endregion
+
+        #region 半角全角相互转换
+        /// <summary>
+        /// 转全角的函数(SBC case)
+        /// </summary>
+        /// <param name="input">需要转换的字符串</param>
+        /// <returns>转换为全角的字符串</returns>
+        public static string ToSBC(this string input)
+        {
+            try
+            {
+                //半角转全角：
+                char[] c = input.ToCharArray();
+                for (int i = 0; i < c.Length; i++)
+                {
+                    if (c[i] == 32)
+                    {
+                        c[i] = (char)12288;
+                        continue;
+                    }
+                    if (c[i] < 127)
+                        c[i] = (char)(c[i] + 65248);
+                }
+                return new string(c);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        /// <summary>
+        ///  转半角的函数(SBC case)
+        /// </summary>
+        /// <param name="input">需要转换的字符串</param>
+        /// <returns>转换为半角的字符串</returns>
+        public static string ToDBC(this string input)
+        {
+            try
+            {
+                char[] c = input.ToCharArray();
+                for (int i = 0; i < c.Length; i++)
+                {
+                    if (c[i] == 12288)
+                    {
+                        c[i] = (char)32;
+                        continue;
+                    }
+                    if (c[i] > 65280 && c[i] < 65375)
+                        c[i] = (char)(c[i] - 65248);
+                }
+                return new string(c);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        #endregion
+
+        #region 检查一个字符串是否是纯数字构成的，一般用于查询字符串参数的有效性验证
+        /// <summary>
+        /// 检查一个字符串是否是纯数字构成的,一般用于查询字符串参数的有效性验证
+        /// </summary>
+        /// <param name="value">需验证的字符串</param>
+        /// <returns>是否合法的bool值。</returns>
+        public static bool IsNumber(this string value)
+        {
+            try
+            {
+                return QuickValidate(value, @"^\d+$");
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        #endregion
+
+        #region 快速验证一个字符串是否符合指定的正则表达式
+        /// <summary>
+        /// 快速验证一个字符串是否符合指定的正则表达式
+        /// </summary>
+        /// <param name="value">需验证的字符串</param>
+        /// <param name="express">正则表达式的内容</param>
+        /// <returns>是否合法的bool值。</returns>
+        public static bool QuickValidate(this string value, string express)
+        {
+            try
+            {
+                if (value == null | value.Length == 0)
+                    return false;
+                Regex myRegex = new Regex(express);
+                return myRegex.IsMatch(value);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
         }
         #endregion
     }
