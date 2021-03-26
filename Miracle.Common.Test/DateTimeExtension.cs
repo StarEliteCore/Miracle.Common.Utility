b86b42d9f002c8435e4d81bfd4801fff6d2e59c9
杜宇 <dygood@outlook.com>
2021-03-26 12:32:15 +0800

    更新日期扩展类的使用.

--- Miracle.Common.Test/DateTimeExtension.cs
+++ Miracle.Common.Test/DateTimeExtension.cs
@@ -18,6 +18,7 @@ namespace Miracle.Common.Test
             var daystart = now.DayStart();
             var dayend = now.DayEnd();
             var (days, daye) = now.DayStartEnd();
+
             var monthstart = now.MonthStart();
             var monthend = now.MonthEnd();
             var (months, monthe) = now.MonthStartEnd();
@@ -33,6 +34,20 @@ namespace Miracle.Common.Test
             WriteColorText($"{dayend:yyyy-MM-dd HH:mm:ss}", ConsoleColor.Magenta);
             Write("某日的开始时间和结束时间:");
             WriteColorText($"{days:yyyy-MM-dd HH:mm:ss} - {daye:yyyy-MM-dd HH:mm:ss}", ConsoleColor.Magenta);
+            Write("某周的开始时间:[周一为一周的第一天]");
+            WriteColorText($"{now.WeekStart(DayOfWeek.Monday):yyyy-MM-dd HH:mm:ss}", ConsoleColor.Magenta);
+            Write("某周的结束时间:");
+            WriteColorText($"{now.WeekEnd(DayOfWeek.Monday):yyyy-MM-dd HH:mm:ss}", ConsoleColor.Magenta);
+            Write("某周的开始时间和结束时间:");
+            var (weeks0, weeke0) = now.WeekStartEnd(DayOfWeek.Monday);
+            WriteColorText($"{weeks0:yyyy-MM-dd HH:mm:ss} - {weeke0:yyyy-MM-dd HH:mm:ss}", ConsoleColor.Magenta);
+            Write("某周的开始时间:[周日为一周的第一天]");
+            WriteColorText($"{now.WeekStart(DayOfWeek.Sunday):yyyy-MM-dd HH:mm:ss}", ConsoleColor.Magenta);
+            Write("某周的结束时间:");
+            WriteColorText($"{now.WeekEnd(DayOfWeek.Sunday):yyyy-MM-dd HH:mm:ss}", ConsoleColor.Magenta);
+            Write("某周的开始时间和结束时间:");
+            var (weeks1, weeke1) = now.WeekStartEnd(DayOfWeek.Sunday);
+            WriteColorText($"{weeks1:yyyy-MM-dd HH:mm:ss} - {weeke1:yyyy-MM-dd HH:mm:ss}", ConsoleColor.Magenta);
             Write("某月的开始时间:");
             WriteColorText($"{monthstart:yyyy-MM-dd HH:mm:ss}", ConsoleColor.Magenta);
             Write("某月的结束时间:");
