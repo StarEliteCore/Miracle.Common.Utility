850f823bd9c25cd68997fc5f83193f96a9c54899
杜宇 <dygood@outlook.com>
2021-03-26 12:21:21 +0800

    日期扩展新增周的起始日期,支持自定义一周的第一天.

--- Miracle.Common.Utility.DateTime/DateTimeExtension.cs
+++ Miracle.Common.Utility.DateTime/DateTimeExtension.cs
@@ -23,11 +23,34 @@ namespace Miracle.Common.Utility.DateTimeExtension
         /// <returns>(Start, End)</returns>
         public static ValueTuple<DateTime, DateTime> DayStartEnd(this DateTime dateTime) => new(dateTime.DayStart(), dateTime.DayEnd());
         /// <summary>
+        /// 获取某天的所属周的开始时间
+        /// </summary>
+        /// <param name="dateTime">某周中的任意天日期</param>
+        /// <param name="firstDay">一周的第一天[周日还是周一或者其他]</param>
+        /// <returns></returns>
+        public static DateTime WeekStart(this DateTime dateTime, DayOfWeek firstDay) => dateTime.AddDays(-dateTime.DayOfWeek.DayNumber()).DayStart().AddDays((int)firstDay);
+
+        /// <summary>
+        /// 获取某天的所属周的结束时间
+        /// </summary>
+        /// <param name="dateTime">某周中的任意天日期</param>
+        /// <param name="firstDay">一周的第一天[周日还是周一或者其他]</param>
+        /// <returns></returns>
+        public static DateTime WeekEnd(this DateTime dateTime, DayOfWeek firstDay) => dateTime.WeekStart(firstDay).AddDays(6).DayEnd();
+        /// <summary>
+        /// 获取某天所属周的开始和结束时间
+        /// </summary>
+        /// <param name="dateTime">某周中的任意天日期</param>
+        /// <param name="firstDay">一周的第一天[周日还是周一或者其他]</param>
+        /// <returns></returns>
+        public static ValueTuple<DateTime, DateTime> WeekStartEnd(this DateTime dateTime, DayOfWeek firstDay) => new(dateTime.WeekStart(firstDay), dateTime.WeekEnd(firstDay));
+
+        /// <summary>
         /// 获取某月的开始时间
         /// </summary>
         /// <param name="dateTime">某月中的任意天日期</param>
         /// <returns></returns>
-        public static DateTime MonthStart(this DateTime dateTime) => dateTime.Date.AddDays(1 - dateTime.Day);
+        public static DateTime MonthStart(this DateTime dateTime) => dateTime.DayStart().AddDays(1 - dateTime.Day);
         /// <summary>
         /// 获取某月的结束时间
         /// </summary>
@@ -45,7 +68,7 @@ namespace Miracle.Common.Utility.DateTimeExtension
         /// </summary>
         /// <param name="dateTime">某年中的任意一天</param>
         /// <returns></returns>
-        public static DateTime YearStart(this DateTime dateTime) => dateTime.Date.AddMonths(1 - dateTime.Month).AddDays(1 - dateTime.Day).Date;
+        public static DateTime YearStart(this DateTime dateTime) => dateTime.Date.AddMonths(1 - dateTime.Month).AddDays(1 - dateTime.Day).DayStart();
         /// <summary>
         /// 获取某年的结束时间
         /// </summary>
---
+++ Miracle.Common.Utility.DateTime/Tools.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Miracle.Common.Utility.DateTimeExtension
+{
+    public static class Tools
+    {
+        /// <summary>
+        /// 获取整周的星期数字形式
+        /// </summary>
+        /// <param name="day"></param>
+        /// <returns></returns>
+        public static int DayNumber(this DayOfWeek day) => day switch
+        {
+            DayOfWeek.Friday => 5,
+            DayOfWeek.Monday => 1,
+            DayOfWeek.Saturday => 6,
+            DayOfWeek.Thursday => 4,
+            DayOfWeek.Tuesday => 2,
+            DayOfWeek.Wednesday => 3,
+            _ => 7,
+        };
+
+        /// <summary>
+        /// 将0-7的数字转化成DayOfWeek类型
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static DayOfWeek ToDayOfWeek(this int number) => number > 7 | number < 0
+                ? throw new("please input 0-7")
+                : number switch
+                {
+                    0 => DayOfWeek.Sunday,
+                    1 => DayOfWeek.Monday,
+                    2 => DayOfWeek.Tuesday,
+                    3 => DayOfWeek.Wednesday,
+                    4 => DayOfWeek.Thursday,
+                    5 => DayOfWeek.Friday,
+                    6 => DayOfWeek.Saturday,
+                    _ => DayOfWeek.Sunday
+                };
+    }
+}
